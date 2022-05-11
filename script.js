a1c6f238cb98131b14f7a8a246c6f053a5d35a7d
Francisco Stenico <francisco.stenico@gmail.com>
2022-05-11 12:51:59 -0300

    up inicial do codigo

---
+++ script.js
@@ -0,0 +1,39 @@
+// JOGO LABIRINTO
+
+function criarElemento(nome, local, classe, id) {
+  const tagNome = document.createElement(nome);
+  const onde = document.querySelector(local);
+
+  if ([!classe, !id].every((i) => !i)) {
+    tagNome.classList.add(classe);
+    tagNome.id = id;
+  } else if (!classe) {
+    tagNome.id = id;
+  } else if (!id) {
+    tagNome.classList.add(classe);
+  }
+  onde.appendChild(tagNome);
+  console.log(onde);
+  return tagNome;
+}
+
+const emoji = criarElemento("div", "body", undefined, "box");
+
+let boxCima = 25;
+let boxEsquerda = 0;
+function moverEmoji(event) {
+  if (event.key == "ArrowDown") {
+    boxCima += 5;
+  } else if (event.key == "ArrowUp") {
+    boxCima -= 5;
+  } else if (event.key == "ArrowRight") {
+    boxEsquerda += 5;
+  } else if (event.key == "ArrowLeft") {
+    boxEsquerda -= 5;
+  }
+
+  emoji.style.top = `${boxCima}px`;
+  emoji.style.left = `${boxEsquerda}px`;
+}
+
+document.addEventListener("keydown", moverEmoji);
